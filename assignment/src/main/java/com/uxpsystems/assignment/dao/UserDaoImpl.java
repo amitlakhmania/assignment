e59f133ec1a48edf4f3840fb3aeea930d83d18ac
purviagrawal <purviagrawal@192.168.43.242>
2019-01-23 00:15:37 +0530

    gitignore

--- assignment/src/main/java/com/uxpsystems/assignment/dao/UserDaoImpl.java
+++ assignment/src/main/java/com/uxpsystems/assignment/dao/UserDaoImpl.java
@@ -1,18 +1,31 @@
 package com.uxpsystems.assignment.dao;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.uxpsystems.assignment.controller.User;
 
 @Repository
 public class UserDaoImpl implements UserDao {
+	
+  @Autowired
+  JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<User> getUsers() {
 		
-		return null;
+		return jdbcTemplate.query("select * from User",new BeanPropertyRowMapper<User>(User.class));
 	}
 
+	public User findById(long id) {
+		return jdbcTemplate.queryForObject("select * from User where id=?", new Object[] { id },
+				new BeanPropertyRowMapper<User>(User.class));
+	}
+	
+	
 }
